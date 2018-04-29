475eb7a4d875d04837d40b4221cd2479d41371e3
ajung <cwhtha@gmail.com>
2018-04-29 18:54:49 +0900

    0429 solitaire(1)

---
+++ Assets/Card.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour {
+	public bool isHidden;
+	public Sprite hidden;
+	private Sprite cardTexture;
+
+	public Card(int i, int j, Sprite texture){
+		isHidden = true;
+		this.cardTexture = texture;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
