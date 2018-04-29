372634391735559ddf008771e8f38f6a199bfdc6
ajung <cwhtha@gmail.com>
2018-04-29 20:01:46 +0900

    0429 solitaire(2)

--- Assets/Card.cs
+++ Assets/Card.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Card : MonoBehaviour {
-	public bool isHidden;
+/*	public bool isHidden;
 	public Sprite hidden;
 	private Sprite cardTexture;
 
 	public Card(int i, int j, Sprite texture){
 		isHidden = true;
 		this.cardTexture = texture;
-	}
+	}*/
 
 	// Use this for initialization
 	void Start () {
