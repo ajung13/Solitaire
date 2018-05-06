639c373e595b2d28899b919ed9a60b56538d5ada
ajung <cwhtha@gmail.com>
2018-05-06 13:40:21 +0900

    0506 study

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour {
+	public bool isHidden;
+	private Sprite cardTexture;
+	private int shape, number;
+	private int line, lineIdx;
+
+	public Card(int i, int j, Sprite texture){
+		isHidden = true;
+		this.cardTexture = texture;
+		this.shape = i;
+		this.number = j;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public enum shape {Heart, Diamond, Clova, Spade};
+
+public class GameController : MonoBehaviour {
+	public Sprite[] shape = new Sprite[4];
+	public Sprite hiddenCard;
+	public Sprite[,] cardSprites = new Sprite[4, 13];
+
+	public GameObject Card;
+	private Card[] playCards = new Card[4*13];
+	private int cardIdx = 0;
+	private bool[] setFlag = new bool[4*13];
+
+	public Vector2[] playDeck = new Vector2[7];
+	public Vector2[] shapeDeck = new Vector2[4];
+
+	// Use this for initialization
+	void Start () {
+		preCardSet ();
+		for (int i = 0; i < 4 * 13; i++)
+			setFlag [i] = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void preCardSet(){
+		for (int i = 0; i < 7; i++) {
+			playDeck [i] = new Vector2 (-3.6f + 1.8f * i, 3.37f);
+			for (int j = 0; j < i; j++)
+				Instantiate (Card, playDeck [i] + new Vector2 (0.0f, -0.3f * j), Quaternion.identity);
+			newCardSet (i, playDeck[i] + new Vector2(0.0f, -0.3f * i));
+		}
+		for (int i = 0; i < 4; i++) {
+			shapeDeck [i] = new Vector2 (-7.5f + (i % 2) * 1.7f, -1 - (i / 2) * 2.3f);
+			GameObject tmp = Instantiate (Card, shapeDeck [i], Quaternion.identity) as GameObject;
+			tmp.GetComponent<SpriteRenderer> ().sprite = shape[i];
+		}
+	}
+
+	void newCardSet(int line, Vector2 position){
+		int newRandomNum = (int) Random.Range (0.0f, 52.0f);
+		while (newRandomNum == 52 || setFlag [newRandomNum / 13, newRandomNum % 13]) {
+			//find another random number;
+			newRandomNum = (int) Random.Range (0.0f, 52.0f);
+		}
+		setFlag [newRandomNum / 13, newRandomNum % 13] = true;
+
+		playCards [cardIdx] = new Card (newRandomNum / 13, newRandomNum % 13, cardSprites[newRandomNum / 13, newRandomNum % 13]);
+		playCards [cardIdx].line = line;
+		playCards [cardIdx].lineIdx = line;
+
+		GameObject tmp = Instantiate (Card, position, Quaternion.identity) as GameObject;
+		tmp.GetComponent<SpriteRenderer> ().sprite = playCards [cardIdx].cardTexture;
+	}
+}
